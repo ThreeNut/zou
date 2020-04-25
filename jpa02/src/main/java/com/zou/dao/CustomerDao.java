efdd81721bee0715362191e3f9344ecc9930c839
ThreeNut <1769040047@qq.com>
2020-04-25 16:31:19 +0800

    springDataJpa 结束

--- jpa02/src/main/java/com/zou/dao/CustomerDao.java
+++ jpa02/src/main/java/com/zou/dao/CustomerDao.java
@@ -3,8 +3,11 @@ package com.zou.dao;
 import com.zou.pojo.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  *
@@ -19,6 +22,7 @@ public interface CustomerDao extends JpaRepository<Customer,Long>, JpaSpecificat
 
     /*  稍微复杂查询需要自定义方法 通过jpql 进行查询(像通用mapper)
     根据客户名称查询客户 jpql from Customer where custName = ?
+      --------------- @Query 代表查询-----------------
     */
     @Query(value = " from Customer where custName = ? ")
     public Customer findByName(String name);
@@ -32,4 +36,37 @@ public interface CustomerDao extends JpaRepository<Customer,Long>, JpaSpecificat
         public Customer findByNameAndId(Long id,String name);
     * */
 
+    //根据id更新 电话  注意由于是自己定义的方法 所以需要添加事务
+    @Query(value = "update Customer set custPhone = ?2 where custId = ?1 ")
+    @Modifying //表示...@Query是更新的操作
+    public  void updateById(Long id,String phone);
+
+    //------------------使用sql语句查询 所有数据-------------
+    @Query(value = "select * from cst_customer ", nativeQuery = true)//nativeQuery 使用本地sql语句查询
+    public List<Object [] > findAllSql();
+
+    //sql语句 条件查询
+    @Query(value = "select * from cst_customer where cust_name like ?",nativeQuery = true)
+    public List<Object []> findBySql(String name);
+
+/*    方法命名规则查询
+*     按照Spring Data JPA 定义的规则，
+* 查询方法以findBy开头，涉及条件查询时，
+* 条件的属性用条件关键字连接，要注意的是：
+* 条件属性首字母需大写。框架在进行方法名解析时，
+* 会先把方法名多余的前缀截取掉，然后对剩下部分进行解析。
+
+* -------------方法命名的约定--------------
+*   findBy : 查询
+*   对象(pojo)中的属性名(首字母大写) : 查询条件
+*   findByCustName --根据客户名查询
+* */
+    public Customer findByCustName(String custName);
+
+    //模糊查询  findBy + pojo属性名 + 查询方式 like
+    public List<Customer> findByCustNameLike(String custName);
+
+    //多条件查询    findBy + pojo属性名 + 查询方式 like 多条件连接符(and|or) + 属性名 + 查询方式
+    //使用客户名称模糊匹配和客户所属行业精准匹配查询
+    public Customer findByCustNameLikeAndCustIndustry(String custName,String custIndustry);
 }
--- jpa02/src/test/java/com/zou/SpringDatajpqlTest.java
+++ jpa02/src/test/java/com/zou/SpringDatajpqlTest.java
@@ -5,10 +5,12 @@ import com.zou.pojo.Customer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 /**
  * 复杂查询 自定义jqpl
@@ -20,6 +22,7 @@ import java.util.List;
 public class SpringDatajpqlTest {
     @Autowired
     private CustomerDao customerDao; //注入
+    //---------------------------jpql查询-------------------------------------------
     //根据客户名称查询对象
     @Test
     public void testFindJpql(){
@@ -32,4 +35,55 @@ public class SpringDatajpqlTest {
         Customer customer = customerDao.findByNameAndId("zou1",1L);
         System.out.println(customer);
     }
+    /*根据id更新 电话
+    注意   1.由于是自己定义的方法 所以需要添加事务@Transactional
+          2. jpa默认回滚事务 所以更新失败   @Rollback
+    * */
+    @Test
+    @Transactional
+    @Rollback(value = false) //关闭事务回滚
+    public void updateById(){
+        customerDao.updateById(1L,"88");
+    }
+//------------------------ sql语句查询-----------------------------
+    //使用本地sql语句查询
+    @Test
+    public void findAllSql(){
+        List<Object[]> allSql = customerDao.findAllSql();
+        for (Object [] objects :allSql){
+            System.out.println(Arrays.toString(objects));
+        }
+    }
+    //使用sql语句进行 条件查询
+    @Test
+    public void findBySql(){
+        List<Object[]> bySql = customerDao.findBySql("zou%");
+        for(Object[] objects:bySql){
+            System.out.println(Arrays.toString(objects));
+        }
+    }
+//-------------------方法命名规则查询------------------------------------
+    /*顾名思义，方法命名规则查询就是根据方法的名字，就能创建查询。
+    只需要按照Spring Data JPA提供的方法命名规则定义方法的名称，就可以完成查询工作。
+    Spring Data JPA在程序执行的时候会根据方法名称进行解析，并自动生成查询语句进行查询*/
+    //查询
+    @Test
+    public void findByName() {
+        Customer zou1 = customerDao.findByCustName("zou1");
+        System.out.println(zou1);
+    }
+    //模糊查询
+    @Test
+    public void findByLike() {
+        List<Customer> byCustNameLike = customerDao.findByCustNameLike("z%");
+        byCustNameLike.forEach(System.out::println);
+    }
+
+    //使用 客户名称模糊匹配  且 客户所属行业精准匹配  查询
+    @Test
+    public void findByLikeMany() {
+        Customer it = customerDao.findByCustNameLikeAndCustIndustry("z%", "it");
+        System.out.println(it);
+    }
 }
+//25集
